43273bf74a5dc1d3d65f6c96a2cb4e7ff29b7a5d
denisbeybutov <beybutov.denis@mail.ru>
2026-04-25 14:11:14 +0300

    все задачи

--- script.js
+++ script.js
@@ -65,23 +65,115 @@
 
 //--------задача 5
 
-const delay = ms => new Promise(r => setTimeout(r, ms));
+// const delay = ms => new Promise(r => setTimeout(r, ms));
 
-const getUser = id => delay(20).then(() => ({ id, name: 'Alice' }));
-const getPosts = uid => delay(20).then(() => [{ id: 1, userId: uid }, { id: 2, userId: uid }]);
-const getFirstPostComments = pid => delay(20).then(() => [{ id: 11, postId: pid, text: 'Hello its great' }]);
+// const getUser = id => delay(20).then(() => ({ id, name: 'Alice' }));
+// const getPosts = uid => delay(20).then(() => [{ id: 1, userId: uid }, { id: 2, userId: uid }]);
+// const getFirstPostComments = pid => delay(20).then(() => [{ id: 11, postId: pid, text: 'Hello its great' }]);
 
-// Написать код, чтобы получить комментарий первого поста
-let userId = getUser(123)
-.then((user) => {
-    console.log(user.id)
-    return getPosts(user.id)
-})
-.then((arrOfPosts) => {
-    console.log(arrOfPosts[0].id)
-    return getFirstPostComments(arrOfPosts[0].id)
-})
-.then((post) => {
-    console.log(post[0].text)
-})
+// // Написать код, чтобы получить комментарий первого поста
+// let userId = getUser(123)
+// .then((user) => {
+//     console.log(user.id)
+//     return getPosts(user.id)
+// })
+// .then((arrOfPosts) => {
+//     console.log(arrOfPosts[0].id)
+//     return getFirstPostComments(arrOfPosts[0].id)
+// })
+// .then((post) => {
+//     console.log(post[0].text)
+// })
 
+//---------------
+
+
+// Promise.resolve("hello")
+//   .then((result) => result + "world")
+//   .then((result) => console.log(result))
+//   .catch((err) => console.error(err));
+
+//--
+
+// Promise.reject("a")
+//   .catch((p) => p + "b")
+//   .catch((p) => p + "c")
+//   .then((p) => p + "d")
+//   .finally((p) => p + "e")
+//   .then((p) => console.log(p));
+
+// Promise.reject("a")
+//   .then(
+//     (p) => p + "1",
+//     (p) => p + "2"
+//   )
+//   .catch((p) => p + "b")
+//   .catch((p) => p + "c")
+//   .then((p) => p + "d1")
+//   .then(() => "d2")
+//   .then((p) => p + "d3")
+//   .finally((p) => p + "e")
+//   .then((p) => console.log(p));
+
+const promise1 = Promise.resolve("Пользователь");
+const promise2 = Promise.reject(new Error("Ошибка загрузки"));
+const promise3 = Promise.resolve("Настройки");
+
+  function allSettled(prom1, prom2, prom3){
+    let arr =[];
+    
+        prom1.then((res1)=>{
+            console.log(res1)
+            arr.push( {
+                status: 'ful',
+                value: res1
+            })
+        }).catch((res1)=> {
+            console.log(res1)
+            arr.push( {
+                status: 'rej',
+                value: res1
+            })
+        })
+        
+
+    
+        prom2.then((res2)=> {
+            console.log(res2)
+            arr.push( {
+                status: 'ful',
+                value: res2
+            })
+        }).catch((res2)=> {
+            console.log(res2)
+            arr.push( {
+                status: 'rej',
+                value: res2
+            })
+        })
+
+        prom3.then((res3)=> {
+            console.log(res3)
+            arr.push( {
+                status: 'ful',
+                value: res3
+            })
+        }).catch((res3)=> {
+            console.log(res3)
+            arr.push( {
+                status: 'rej',
+                value: res3
+            })
+        })
+    
+    
+    return arr;
+  }
+
+console.log(allSettled(promise1, promise2, promise3))
+
+
+// Promise.allSettled([promise1, promise2, promise3])
+//   .then((results) => {
+//     console.log(results);
+//   });
