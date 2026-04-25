8325f4a8b8443fcb61a7beabf6d1ba2a68e34f86
denisbeybutov <beybutov.denis@mail.ru>
2026-04-25 07:10:06 +0300

    вторая задача готова

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
-function delayedAdd(a, b, delay, callback) {
-    setTimeout(() => {
+//---
+
+// function delayedAdd(a, b, delay, callback) {
+//     setTimeout(() => {
          
-        callback(null, a+b)
+//         callback('its error', a+b)
          
-    }, delay)
+//     }, delay)
     
+// }
+
+// delayedAdd(7, 3, 50, (err, sum) => {
+//   if (err) return console.error(err);
+//   console.log('sum =', sum); // 5
+// });
+
+
+//---------
+
+function readConfig(path, callback) {
+    setTimeout(()=> {
+        if(typeof path !== 'string') callback('Error: path is not string')
+        else callback(null, {'path': path})
+    },1000)
 }
 
-delayedAdd(7, 3, 50, (err, sum) => {
-  if (err) return console.error(err);
-  console.log('sum =', sum); // 5
-});
+readConfig('/etc/app.json', (err, cfg) =>{
+    console.log(err ? err.message : cfg)
+    // {path: '/etc/app.json }  
+}) 
+readConfig(42, (err) => console.log(err)); // Error
 
-function nex() {
-
-}
+//--------
