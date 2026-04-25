f4d1f4cbca6eb0d2782b71daa06df183be34873e
denisbeybutov <beybutov.denis@mail.ru>
2026-04-25 07:30:03 +0300

    третья задача готова

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//---
+//--- задача 1
 
 // function delayedAdd(a, b, delay, callback) {
 //     setTimeout(() => {
@@ -15,19 +15,31 @@
 // });
 
 
-//---------
+//--------- Задача 2
 
-function readConfig(path, callback) {
-    setTimeout(()=> {
-        if(typeof path !== 'string') callback('Error: path is not string')
-        else callback(null, {'path': path})
-    },1000)
+// function readConfig(path, callback) {
+//     setTimeout(()=> {
+//         if(typeof path !== 'string') callback('Error: path is not string')
+//         else callback(null, {'path': path})
+//     },1000)
+// }
+
+// readConfig('/etc/app.json', (err, cfg) =>{
+//     console.log(err ? err.message : cfg)
+//     // {path: '/etc/app.json }  
+// }) 
+// readConfig(42, (err) => console.log(err)); // Error
+
+//--------задача 3
+
+function addAsync(a, b, delay = 1000) {
+    return new Promise ((resolve,reject) => {
+        setTimeout(() => {
+            console.log('сумма через секунду');
+            const sum = a + b;
+            resolve(sum);
+        }, delay)
+    })
 }
 
-readConfig('/etc/app.json', (err, cfg) =>{
-    console.log(err ? err.message : cfg)
-    // {path: '/etc/app.json }  
-}) 
-readConfig(42, (err) => console.log(err)); // Error
-
-//--------
+addAsync(2, 3).then(x => console.log(x)); // 5
